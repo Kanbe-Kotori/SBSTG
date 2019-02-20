b96b1d0837d9668c8d13ee11feb8421e7cf57c56
Kanbe Kotori <1438726022@qq.com>
2019-02-20 23:55:28 +0800

    MissileGenerator
    zsbd

---
+++ src/entity/missile/MissileGenerator.ts
@@ -0,0 +1,14 @@
+class MissileGenerator {
+
+    public static createSniperMissile(point:egret.Point, velocity:number, size:number, color:number) {
+        let x = SelfMachine.INSTANCE.getX();
+        let y = SelfMachine.INSTANCE.getY();
+        let dx = x - point.x;
+        let dy = y - point.y;
+        let vx = velocity * dx / Math.sqrt(dx * dx + dy * dy)
+        let vy = velocity * dy / Math.sqrt(dx * dx + dy * dy)
+        let missile = new StandardMissile(point, vx,vy, size, color);
+        return missile;
+    }
+    
+}
