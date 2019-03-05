84a310d72f7e21fcd673023ea7748ac77e756311
Kanbe Kotori <1438726022@qq.com>
2019-03-05 21:10:18 +0800

    按钮
    和自机大小

--- src/scene/control/Button.ts
+++ src/scene/control/Button.ts
@@ -45,5 +45,17 @@ class Button extends egret.Sprite {
         this.img.y = this._pos.y;
         this.addChild(this.img);
     }
+	
+    public static return() {
+        let current = SelfMachine.INSTANCE.currentStage;
+        current.end();
+        Main.getMain().removeChild(current);
+        Main.getMain().addChild(PageMain.INSTANCE);
+    }
+
+	public static restart() {
+		let current = SelfMachine.INSTANCE.currentStage;
+		current.restart();
+	}
 
 }
