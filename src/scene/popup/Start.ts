572705ba8f04030f9d3c578f37fb0c4887421666
Nyx <weiyichen007@163.com>
2019-05-20 22:16:00 +0800

    弹窗优化

--- src/scene/popup/Start.ts
+++ src/scene/popup/Start.ts
@@ -15,26 +15,18 @@ class Start extends Popup {
     protected doRender() {
         super.doRender();
 		this._img = MyUtils.createBitmapByName(TextureNames.POPUP_START);
-        this._img.width = 600;
-        this._img.height = 600;
+        this._img.width = 800;
+        this._img.height = 800;
         this._img.anchorOffsetX = this._img.width/2;
         this._img.anchorOffsetY = this._img.height/2;
         this._img.x = Main.X/2;
         this._img.y = Main.Y/2;
         this.addChild(this._img);
 
-		this.btnRestart = new Button(120, 120, new egret.Point(360, 1140), TextureNames.BUTTON_RESTART);
+		this.btnRestart = new Button(120, 120, new egret.Point(540, 1240), TextureNames.BUTTON_RESUME);
         this.btnRestart.setAction(Start.click_restart);
         this.addChild(this.btnRestart);
 
-		this.btnInfo = new Button(120, 120, new egret.Point(540, 1140), TextureNames.BUTTON_INFO);
-        this.btnInfo.setAction(Start.click_info);
-        this.addChild(this.btnInfo);
-
-		this.btnReturn = new Button(120, 120, new egret.Point(720, 1140), TextureNames.BUTTON_RETURN);
-        this.btnReturn.setAction(Start.click_return);
-        this.addChild(this.btnReturn);
-        
     }
 
 	public static click_info(evt:egret.TouchEvent) {
