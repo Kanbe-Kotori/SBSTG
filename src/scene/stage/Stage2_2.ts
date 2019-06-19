890dcb962e3302c10321eeb4ef5d7aa8748ce692
Kanbe Kotori <1438726022@qq.com>
2019-06-19 17:10:50 +0800

    2-2meijiaoshangqu

---
+++ src/scene/stage/Stage2_2.ts
@@ -0,0 +1,61 @@
+class Stage2_2 extends StageBase {
+    public static readonly INSTANCE:Stage2_2 = new Stage2_2("2-2", 30);
+
+    protected initEmitters() {
+        let rain = new SideEmitterUpgrade(
+            new MissileConfig(MissileUtils.MISSILE_ROUND)
+                .setVelocity(15)
+                .setExtraPara(MissileUtils.RANDOM_VELOCITY_PARA, 20)
+                .setTexture(TextureNames.MISSILE_BLUE)
+                .addHandler(
+                    new EdgeEventHandler(
+                        (missile:MissileBase) => {
+                            let side = missile.getEdge();
+                            if (side == Side.LEFT) {
+                                missile._img.x += Main.X;
+                            } else if (side == Side.RIGHT) {
+                                missile._img.x -= Main.X;
+                            } else {
+                                missile.setDead();
+                            }
+                        }
+                    )
+                )
+            )
+        .setFreq(250)
+        .setStartAngle(75)
+        .setEndAngle(105)
+        .setNumber(8);
+
+		let point2 = new egret.Point(Main.X * 0.4, 420);
+		let em2 = new EmptyEmitter().setPos(point2);
+        let up2_1 = new RenderUpgrade(TextureNames.FLOWER1, 120, 90).setParentEmitter(em2).renderOnStage(this);
+        let up2_2 = new SniperUpgrade(
+            new MissileConfig(MissileUtils.MISSILE_ELLIPTICAL)
+                .setSize(30, 36)
+                .setTexture(TextureNames.MISSILE_PETAL2)
+                .setVelocity(15)
+        )
+        .setParentEmitter(em2)
+        .setNumber(7)
+        .setStep(30)
+        .setDiv(1)
+        .setFreq(300);
+
+		let point3 = new egret.Point(Main.X * 0.6, 420);
+		let em3 = new EmptyEmitter().setPos(point3);
+        let up3_1 = new RenderUpgrade(TextureNames.FLOWER1, 120, 90).setParentEmitter(em3).renderOnStage(this);
+        let up3_2 = new SniperUpgrade(
+            new MissileConfig(MissileUtils.MISSILE_ELLIPTICAL)
+                .setSize(30, 36)
+                .setTexture(TextureNames.MISSILE_PETAL2)
+                .setVelocity(15)
+        )
+        .setParentEmitter(em3)
+        .setNumber(7)
+        .setStep(30)
+        .setDiv(1)
+        .setFreq(300);
+    }
+
+}
